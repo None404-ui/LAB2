6a08feac0064fe1d6cc5eb6705bb0c1c7dc5a7ec
andrej fokin <v3nam06@gmail.com>
2025-09-30 17:21:54 +0400

    Создание интерфейса MathFunction

--- src/main/java/functions/MathFunction.java
+++ src/main/java/functions/MathFunction.java
@@ -3,6 +3,9 @@ package functions;
 public interface MathFunction {
     double apply(double x);
     default CompositeFunction andThen(MathFunction afterFunction) {
+        if (afterFunction == null) {
+            throw new NullPointerException("After function cannot be null");
+        }
         return new CompositeFunction(this, afterFunction);
     }
 }
