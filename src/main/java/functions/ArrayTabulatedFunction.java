b112a6a41218d12cdc9d3f1c7ff572cd4940ef61
Андрей <v3nam06@gmail.com>
2025-10-13 00:42:57 +0400

    Переписывание реализации метода iterator() в классе ArrayTabulatedFunction

--- src/main/java/functions/ArrayTabulatedFunction.java
+++ src/main/java/functions/ArrayTabulatedFunction.java
@@ -2,6 +2,7 @@ package functions;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Табулированная функция на основе массивов.
@@ -223,6 +224,23 @@ public class ArrayTabulatedFunction extends AbstractTabulatedFunction implements
 
     @Override
     public Iterator<Point> iterator() {
-        throw new UnsupportedOperationException();
+        return new Iterator<Point>() {
+            private int i = 0;
+
+            @Override
+            public boolean hasNext() {
+                return i < count;
+            }
+
+            @Override
+            public Point next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Point point = new Point(xValues[i], yValues[i]);
+                i++;
+                return point;
+            }
+        };
     }
 }
--- src/test/java/functions/IteratorTest.java
+++ src/test/java/functions/IteratorTest.java
@@ -2,6 +2,9 @@ package functions;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -10,13 +13,48 @@ import static org.junit.jupiter.api.Assertions.*;
 public class IteratorTest {
 
     @Test
-    public void testArrayTabulatedFunction_IteratorThrowsException() {
+    public void testArrayTabulatedFunction_IteratorWithForEach() {
         double[] xValues = {1.0, 2.0, 3.0};
         double[] yValues = {1.0, 4.0, 9.0};
         ArrayTabulatedFunction function = new ArrayTabulatedFunction(xValues, yValues);
         
-        assertThrows(UnsupportedOperationException.class, () -> {
-            function.iterator();
+        int i = 0;
+        for (Point point : function) {
+            assertEquals(xValues[i], point.x, 0.001);
+            assertEquals(yValues[i], point.y, 0.001);
+            i++;
+        }
+        assertEquals(3, i);
+    }
+
+    @Test
+    public void testArrayTabulatedFunction_IteratorWithWhile() {
+        double[] xValues = {1.0, 2.0, 3.0};
+        double[] yValues = {1.0, 4.0, 9.0};
+        ArrayTabulatedFunction function = new ArrayTabulatedFunction(xValues, yValues);
+        
+        Iterator<Point> iterator = function.iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            Point point = iterator.next();
+            assertEquals(xValues[i], point.x, 0.001);
+            assertEquals(yValues[i], point.y, 0.001);
+            i++;
+        }
+        assertEquals(3, i);
+    }
+
+    @Test
+    public void testArrayTabulatedFunction_IteratorNoSuchElementException() {
+        double[] xValues = {1.0, 2.0};
+        double[] yValues = {1.0, 4.0};
+        ArrayTabulatedFunction function = new ArrayTabulatedFunction(xValues, yValues);
+        
+        Iterator<Point> iterator = function.iterator();
+        iterator.next();
+        iterator.next();
+        assertThrows(NoSuchElementException.class, () -> {
+            iterator.next();
         });
     }
 
