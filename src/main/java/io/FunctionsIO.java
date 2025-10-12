2dac8b0c687a26e63488fa8543e8aa209be1612a
Андрей <v3nam06@gmail.com>
2025-10-13 00:53:31 +0400

    Добавлен пакет io с классом FunctionsIO (без возможности создания экземпляров)

---
+++ src/main/java/io/FunctionsIO.java
@@ -0,0 +1,17 @@
+package io;
+
+/**
+ * Класс для ввода/вывода табулированных функций
+ * Не может иметь наследников и экземпляров
+ */
+public final class FunctionsIO {
+    
+    /**
+     * Приватный конструктор, запрещающий создание экземпляров
+     * @throws UnsupportedOperationException всегда
+     */
+    private FunctionsIO() {
+        throw new UnsupportedOperationException();
+    }
+}
+
