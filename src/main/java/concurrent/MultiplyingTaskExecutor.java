42483b8e68795711200f5fc07495a7ce0b605543
andrej fokin <v3nam06@gmail.com>
2025-10-28 11:49:24 +0400

    Создан класс MultiplyingTaskExecutor  в пакете concurrent

---
+++ src/main/java/concurrent/MultiplyingTaskExecutor.java
@@ -0,0 +1,32 @@
+package concurrent;
+
+import functions.LinkedListTabulatedFunction;
+import functions.UnitFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiplyingTaskExecutor {
+    public static void main(String[] args) {
+        LinkedListTabulatedFunction function = new LinkedListTabulatedFunction(new UnitFunction(), 1, 1000, 1000);
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            MultiplyingTask task = new MultiplyingTask(function);
+            Thread thread = new Thread(task);
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println(function);
+    }
+}
