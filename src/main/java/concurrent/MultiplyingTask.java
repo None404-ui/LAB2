3c9cd65a48dcd67eb066ca477a5c5fbb7dc39a9a
andrej fokin <v3nam06@gmail.com>
2025-10-28 11:46:59 +0400

    Добавлен класс MultiplyingTask в пакет concurrent

---
+++ src/main/java/concurrent/MultiplyingTask.java
@@ -0,0 +1,19 @@
+package concurrent;
+
+import functions.TabulatedFunction;
+
+public class MultiplyingTask implements Runnable {
+    private final TabulatedFunction function;
+
+    public MultiplyingTask(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < function.getCount(); i++) {
+            function.setY(i, function.getY(i) * 2);
+        }
+        System.out.printf("Поток %s закончил выполнение задачи%n", Thread.currentThread().getName());
+    }
+}
