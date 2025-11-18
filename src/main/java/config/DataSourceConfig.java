fc11582d8634e0a4d706feb4624bcaf87ed4f3ce
Андрей <v3nam06@gmail.com>
2025-11-18 16:30:24 +0400

    Изменения конфигов для корреткной работы с docker

--- src/main/java/config/DataSourceConfig.java
+++ src/main/java/config/DataSourceConfig.java
@@ -2,33 +2,44 @@ package config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
 
 @Configuration
 public class DataSourceConfig {
 
+    private final Environment environment;
+
+    public DataSourceConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public DataSource dataSource() {
-        String activeProfile = System.getProperty("spring.profiles.active");
-        boolean isTest = "test".equals(activeProfile);
-        
+        boolean isTest = Arrays.asList(environment.getActiveProfiles()).contains("test");
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        
+
         if (isTest) {
             dataSource.setDriverClassName("org.h2.Driver");
             dataSource.setUrl("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
             dataSource.setUsername("sa");
             dataSource.setPassword("");
         } else {
-            dataSource.setDriverClassName("org.postgresql.Driver");
-            dataSource.setUrl("jdbc:postgresql://localhost:5432/laba");
-            dataSource.setUsername("postgres");
-            dataSource.setPassword("admin");
+            String driver = environment.getProperty("spring.datasource.driver-class-name", "org.postgresql.Driver");
+            String url = environment.getProperty("spring.datasource.url", "jdbc:postgresql://localhost:5432/laba");
+            String username = environment.getProperty("spring.datasource.username", "postgres");
+            String password = environment.getProperty("spring.datasource.password", "admin");
+
+            dataSource.setDriverClassName(driver);
+            dataSource.setUrl(url);
+            dataSource.setUsername(username);
+            dataSource.setPassword(password);
         }
-        
+
         return dataSource;
     }
 }
-
--- src/main/java/config/JpaConfig.java
+++ src/main/java/config/JpaConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -13,6 +14,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
 import java.util.Properties;
 
 @Configuration
@@ -21,8 +23,14 @@ import java.util.Properties;
 @ComponentScan(basePackages = {"entities", "repositories", "services"})
 public class JpaConfig {
 
+    private final DataSource dataSource;
+    private final Environment environment;
+
     @Autowired
-    private DataSource dataSource;
+    public JpaConfig(DataSource dataSource, Environment environment) {
+        this.dataSource = dataSource;
+        this.environment = environment;
+    }
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
@@ -34,18 +42,23 @@ public class JpaConfig {
         em.setJpaVendorAdapter(vendorAdapter);
 
         Properties properties = new Properties();
-        String activeProfile = System.getProperty("spring.profiles.active");
-        boolean isTest = "test".equals(activeProfile);
-        
-        if (isTest) {
-            properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-            properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-        } else {
-            properties.setProperty("hibernate.hbm2ddl.auto", "update");
-            properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        }
-        properties.setProperty("hibernate.show_sql", "true");
-        properties.setProperty("hibernate.format_sql", "true");
+        boolean isTest = Arrays.asList(environment.getActiveProfiles()).contains("test");
+
+        String ddlAuto = isTest
+                ? "create-drop"
+                : environment.getProperty("spring.jpa.hibernate.ddl-auto", "update");
+        String dialect = isTest
+                ? "org.hibernate.dialect.H2Dialect"
+                : environment.getProperty("spring.jpa.properties.hibernate.dialect",
+                "org.hibernate.dialect.PostgreSQLDialect");
+        String showSql = environment.getProperty("spring.jpa.show-sql", "true");
+        String formatSql = environment.getProperty("spring.jpa.properties.hibernate.format_sql", "true");
+
+        properties.setProperty("hibernate.hbm2ddl.auto", ddlAuto);
+        properties.setProperty("hibernate.dialect", dialect);
+        properties.setProperty("hibernate.show_sql", showSql);
+        properties.setProperty("hibernate.format_sql", formatSql);
+
         em.setJpaProperties(properties);
 
         return em;
@@ -58,4 +71,3 @@ public class JpaConfig {
         return transactionManager;
     }
 }
-
